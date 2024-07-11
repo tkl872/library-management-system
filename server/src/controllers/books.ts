28740a0fa90831bf47ac17b10af81b8e0732e60b
tkl872 <80868052+tkl872@users.noreply.github.com>
2024-07-11 16:32:14 +0800

    Update books.ts

--- server/src/controllers/books.ts
+++ server/src/controllers/books.ts
@@ -83,15 +83,21 @@ class BookController {
     }
 
     public async searchBooks(query: string): Promise<Book[]> {
-        const books = await collections?.books?.find(
-            {
-                $or: [
-                    {title: {$regex: new RegExp(query, 'i')}},
-                    {'authors.name': {$regex: new RegExp(query, 'i')}},
-                ]
-            }).limit(25).toArray();
-        return books;
+  const vector = await getEmbeddings(query);
+  const aggregationPipeline = [
+    {
+      $vectorSearch: {
+        queryVector:  vector,
+        path: 'embeddings',
+        numCandidates: 100,
+        index: 'vectorsearch',
+        limit: 100,
+      }
     }
+  ];
+  const books = await collections?.books?.aggregate(aggregationPipeline).toArray() as Book[];
+  return books;
+}
 
     public async createBook(book: Book): Promise<InsertOneResult> {
         const result = await collections?.books?.insertOne(book);
